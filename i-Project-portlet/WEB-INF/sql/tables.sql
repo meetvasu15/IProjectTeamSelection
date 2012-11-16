0fb921717d22e30045e0592feb9cadab500416ce
shruthi <shruti.mss@gmail.com>
2012-11-16 14:55:37 -0700

    committing Add Project portlet
    1st version of Add Project

---
+++ i-Project-portlet/WEB-INF/sql/tables.sql
@@ -0,0 +1,7 @@
+create table project_Projectdetail (
+	projectID LONG not null primary key,
+	projectTitle VARCHAR(75) null,
+	projectdescription VARCHAR(75) null,
+	contact VARCHAR(75) null,
+	dateAdded DATE null
+);
