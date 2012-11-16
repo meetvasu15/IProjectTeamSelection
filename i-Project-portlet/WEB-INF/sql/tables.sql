d1ed2e6cb7518b05433a4319d384e551db41c392
shruthi <shruti.mss@gmail.com>
2012-11-16 15:04:41 -0700

    Revert "committing Add Project portlet"
    This reverts commit 0fb921717d22e30045e0592feb9cadab500416ce.

--- i-Project-portlet/WEB-INF/sql/tables.sql
+++
@@ -1,7 +0,0 @@
-create table project_Projectdetail (
-	projectID LONG not null primary key,
-	projectTitle VARCHAR(75) null,
-	projectdescription VARCHAR(75) null,
-	contact VARCHAR(75) null,
-	dateAdded DATE null
-);
